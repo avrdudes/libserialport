c27ef1175e02a8c84b9e9117a8675da0e8f5909f
Hans Ulrich Niedermann <hun@n-dimensional.de>
2024-02-26 05:45:37 +0100

    XXX test-builds.sh

--- test-builds.sh
+++ test-builds.sh
@@ -7,16 +7,28 @@ cd "$(dirname "$0")"
 rm -rf autom4te.cache
 autoreconf -vis
 
-rm -rf _cmb
+for d in _{am,cm}{b,d}
+do
+    test -d "$d" || continue
+    chmod -R +w "$d"
+    rm -rf "$d"
+done
+
 cmake -S . -B _cmb -D CMAKE_INSTALL_PREFIX:PATH=/usr/local
 cmake --build _cmb --verbose
 cmake --build _cmb --target install DESTDIR="$PWD/_cmd"
 
 mkdir _amb
 cd _amb
-../configure --prefix=/usr/local
-make -j$(nproc)
+configure_args=""
+if test -d /usr/lib64; then
+    configure_args="$configure_args --libdir=\${exec_prefix}/lib64"
+fi
+../configure --prefix=/usr/local ${configure_args}
+make -j$(nproc) V=1
 make install DESTDIR="$PWD/../_amd"
+make -j$(nproc) doc
+make -j$(nproc) distcheck
 
 cd ..
 
